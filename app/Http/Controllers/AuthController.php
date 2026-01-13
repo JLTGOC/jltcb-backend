c34b7e4ab6ef043301c1e3f987bad5aae427c6dc
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2026-01-13 14:03:55 +0800

    Add validation to AuthController and remove migration
    Added request validation for email and password fields in the register and login methods of AuthController. Also removed the unused personal_access_tokens migration file.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,35 +10,63 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+
+    /**
+     * Register User
+     * 
+     * Register a new user
+     */
     public function register (Request $request) {
-        DB::beginTransaction();
-        try {
-            $newUser = User::create([
-                'email' => $request->email,
-                'password' => Hash::make($request->password)
-            ]);
-            DB::commit();
-            return $this->success('User registered', $newUser, 200);
-        } catch(\Exception $e) {
-            DB::rollback();
-            return $this->error('Something went wrong', 400, $e);
+        $validated = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        if ($validated) {
+            DB::beginTransaction();
+            try {
+                $newUser = User::create([
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password)
+                ]);
+                DB::commit();
+                return $this->success('User registered', $newUser, 200);
+            } catch(\Exception $e) {
+                DB::rollback();
+                return $this->error('Something went wrong', 400, $e);
+            }
         }
-        
     }
 
+    /**
+     * Login
+     * 
+     * Login to registered account
+     */
     public function login (Request $request) {
         $credentials = $request->only('email', 'password');
         
-        if (!auth()->attempt($credentials)) {
-            return $this->error('Invalid credentials', 400);
-        }
+        $validated = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
 
-        $user = auth()->user();
-        $token = $user->createToken('auth_token')->plainTextToken;
-        
-        return $this->success('Logged in successfully', ['user' => $user, 'token' => $token]);
+        if ($validated) {
+            if (!auth()->attempt($credentials)) {
+                return $this->error('Invalid credentials', 400);
+            }
+
+            $user = auth()->user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+            
+            return $this->success('Logged in successfully', ['user' => $user, 'token' => $token]);
+        }
     }
 
+    /**
+     * Logout
+     * 
+     * Logout of authenticated account
+     */
     public function logout (Request $request) {
         $user = $request->user();
 
