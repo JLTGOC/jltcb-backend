8f925b32b00bc915af0b177a53a40c0563ee7523
ignotum-dev <flores.geraldivan@gmail.com>
2026-01-28 10:00:48 +0800

    Enhance seeders by implementing file cleanup and dynamic file copying for articles, reels, and users

--- database/seeders/ReelSeeder.php
+++ database/seeders/ReelSeeder.php
@@ -3,47 +3,58 @@
 namespace Database\Seeders;
 
 use App\Models\Reel;
+use Database\Seeders\Traits\SeederFileTrait;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ReelSeeder extends Seeder
 {
+    use SeederFileTrait;
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
+        // Clean up old files from public storage
+        $this->cleanupSeederFiles('reels');
+
+        $videos = [
+            'DID YOU KNOW - CHESCA.mp4',
+            'ONE WRONG CUSTOMSCODE - CHESCA.mov',
+        ];
+
         $reels = [
             [
-                'video_path' => 'seeders/reels/DID YOU KNOW - CHESCA.mp4',
+                'video_path' => $this->copySeederFile('reels', $videos[0]),
                 'view_count' => 1250,
             ],
             [
-                'video_path' => 'seeders/reels/ONE WRONG CUSTOMSCODE - CHESCA.mov',
+                'video_path' => $this->copySeederFile('reels', $videos[1]),
                 'view_count' => 2340,
             ],
             [
-                'video_path' => 'seeders/reels/DID YOU KNOW - CHESCA.mp4',
+                'video_path' => $this->copySeederFile('reels', $videos[0]),
                 'view_count' => 1890,
             ],
             [
-                'video_path' => 'seeders/reels/ONE WRONG CUSTOMSCODE - CHESCA.mov',
+                'video_path' => $this->copySeederFile('reels', $videos[1]),
                 'view_count' => 3100,
             ],
             [
-                'video_path' => 'seeders/reels/DID YOU KNOW - CHESCA.mp4',
+                'video_path' => $this->copySeederFile('reels', $videos[0]),
                 'view_count' => 2750,
             ],
             [
-                'video_path' => 'seeders/reels/ONE WRONG CUSTOMSCODE - CHESCA.mov',
+                'video_path' => $this->copySeederFile('reels', $videos[1]),
                 'view_count' => 1980,
             ],
             [
-                'video_path' => 'seeders/reels/DID YOU KNOW - CHESCA.mp4',
+                'video_path' => $this->copySeederFile('reels', $videos[0]),
                 'view_count' => 2200,
             ],
             [
-                'video_path' => 'seeders/reels/ONE WRONG CUSTOMSCODE - CHESCA.mov',
+                'video_path' => $this->copySeederFile('reels', $videos[1]),
                 'view_count' => 1450,
             ],
         ];
