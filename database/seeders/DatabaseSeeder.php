1062dcba34eeccbea62e749b3bc638a93b26b74c
ignotum-dev <flores.geraldivan@gmail.com>
2026-02-03 16:32:10 +0800

    Enhance DatabaseSeeder: Implement directory clearing and recreation for storage/app/public

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // Clear storage/app/public directory
+        $storagePath = storage_path('app/public');
+        if (File::isDirectory($storagePath)) {
+            File::deleteDirectory($storagePath);
+            File::makeDirectory($storagePath);
+        }
+
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
