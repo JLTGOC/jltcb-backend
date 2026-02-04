2376f681ab5457018a70bf279ace88733061ed87
ignotum-dev <flores.geraldivan@gmail.com>
2026-02-04 15:32:14 +0800

    Extend Controller class to include authorization capabilities

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 use App\Traits\{
     ResponseAPI
 };
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
-abstract class Controller
+abstract class Controller extends BaseController
 {
     use ResponseAPI;
+    use AuthorizesRequests;
 }
