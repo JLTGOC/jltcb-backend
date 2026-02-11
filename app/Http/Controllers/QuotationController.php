92e3d9f8e05cf9d55b6f4c225ef694321600aa09
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2026-02-11 10:05:24 +0800

    Include client name in quotation search

--- app/Http/Controllers/QuotationController.php
+++ app/Http/Controllers/QuotationController.php
@@ -71,11 +71,19 @@ class QuotationController extends Controller
                 ->filter()
                 ->values();
 
-            if ($searchIds->isEmpty()) {
+            $clientSearchIds = Quotation::query()
+                ->leftJoin('users', 'quotations.client_id', '=', 'users.id')
+                ->where('users.full_name', 'like', "%{$search}%")
+                ->select('quotations.id')
+                ->pluck('id');
+
+            $mergedIds = $searchIds->merge($clientSearchIds)->unique()->values();
+
+            if ($mergedIds->isEmpty()) {
                 return $this->success('No quotations found', [], 200);
             }
 
-            $quotations->whereIn('id', $searchIds);
+            $quotations->whereIn('id', $mergedIds);
         }
 
         $results = $quotations->get();
