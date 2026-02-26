48fb78bcb436a20346634646ea1011bc8e5b835a
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2026-02-26 09:47:31 +0800

    Typecast user ids

--- app/Policies/QuotationFilePolicy.php
+++ app/Policies/QuotationFilePolicy.php
@@ -14,7 +14,7 @@ class QuotationFilePolicy
      */
     public function viewAny(User $user, Quotation $quotation): bool
     {
-        return $user->id === $quotation->client_id || $user->id === $quotation->as_id;
+        return (int) $user->id === (int) $quotation->client_id || (int) $user->id === (int) $quotation->as_id;
     }
 
     /**
@@ -22,7 +22,7 @@ class QuotationFilePolicy
      */
     public function view(User $user, Quotation $quotation, QuotationFile $quotationFile): bool
     {
-        return $user->id === $quotation->client_id || $user->id === $quotation->as_id;
+        return (int) $user->id === (int) $quotation->client_id || (int) $user->id === (int) $quotation->as_id;
     }
 
     /**
