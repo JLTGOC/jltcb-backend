246ee78fcf0e48d6431767848453e15bd2e00071
coderil <178494652+coderil@users.noreply.github.com>
2026-02-27 16:36:16 +0800

    Update scramble docs configuration to add description about Platform header

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,10 @@ namespace App\Providers;
 
 use App\Support\Scramble\SanctumAuthOperationTransformer;
 use Dedoc\Scramble\Configuration\OperationTransformers;
+use Dedoc\Scramble\Support\Generator\Operation;
+use Dedoc\Scramble\Support\Generator\Parameter;
+use Dedoc\Scramble\Support\Generator\Schema;
+use Dedoc\Scramble\Support\Generator\Types\StringType;
 use Dedoc\Scramble\Scramble;
 use Dedoc\Scramble\Support\Generator\OpenApi;
 use Dedoc\Scramble\Support\Generator\SecurityScheme;
@@ -41,6 +45,18 @@ class AppServiceProvider extends ServiceProvider
         Scramble::configure()->withOperationTransformers(function (OperationTransformers $transformers) {
             $transformers->append(SanctumAuthOperationTransformer::class);
         });
+
+        Scramble::configure()
+        ->withOperationTransformers(function (Operation $operation) {
+            $operation->addParameters([
+                Parameter::make('Platform', 'header')
+                    ->description("Use this header to toggle between mobile and web-specific response data. Defaults to mobile if omitted.")
+                    ->setSchema(Schema::fromType(new StringType()))
+                    ->required(false)
+                    ->example('web / mobile'),
+            ]);
+        });
+
 
         // Tie ChatPolicy to Conversation model
         Gate::policy(Conversation::class, ChatPolicy::class);
