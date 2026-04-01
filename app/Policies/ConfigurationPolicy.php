881192cf0b1f5efda19dd3793f20617e3836efb4
coderil <178494652+coderil@users.noreply.github.com>
2026-04-01 13:04:48 +0800

    Add Policy for template authorization

--- app/Policies/ConfigurationPolicy.php
+++ app/Policies/ConfigurationPolicy.php
@@ -2,7 +2,6 @@
 
 namespace App\Policies;
 
-use App\Models\DetailsConfiguration;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Gate;
 use App\Models\Conversation;
 use App\Models\DetailsConfiguration;
 use App\Models\MessageTemplate;
+use App\Models\QuotationTemplate;
 use App\Models\StandardConfiguration;
 use App\Policies\ChatPolicy;
 use App\Policies\ConfigurationPolicy;
@@ -69,6 +70,7 @@ class AppServiceProvider extends ServiceProvider
         Gate::policy(DetailsConfiguration::class, ConfigurationPolicy::class);
         Gate::policy(MessageTemplate::class, ConfigurationPolicy::class);
         Gate::policy(StandardConfiguration::class, ConfigurationPolicy::class);
+        Gate::policy(QuotationTemplate::class, ConfigurationPolicy::class);
         Gate::policy(Role::class, RolePolicy::class);
     }
 }
