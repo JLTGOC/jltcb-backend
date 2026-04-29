670e01edcce7bdccb8197fb1a8bb6e8411d5352c
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2026-04-29 12:20:44 +0800

    Include current_page and total_pages in pagination data

--- app/Traits/Pagination.php
+++ app/Traits/Pagination.php
@@ -10,6 +10,8 @@ trait Pagination
     protected function pagePaginationData(LengthAwarePaginator $paginated): array
     {
         return [
+            'current_page' => $paginated->currentPage(),
+            'total_pages' => $paginated->lastPage(),
             'count' => $paginated->count(),
             'per_page' => $paginated->perPage(),
             'total' => $paginated->total(),
