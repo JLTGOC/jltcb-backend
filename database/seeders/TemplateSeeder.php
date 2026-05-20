9960b8d74aaf54e53a328fb5626e35c413b7ba78
franlyy <reillefrane@gmail.com>
2026-05-20 15:37:21 +0800

    Add regulatory-typed template in TemplateSeeder

--- database/seeders/TemplateSeeder.php
+++ database/seeders/TemplateSeeder.php
@@ -16,9 +16,14 @@ class TemplateSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->createTemplate('LOGISTICS', 'Sample Logistics Template');
+        $this->createTemplate('REGULATORY', 'Sample Regulatory Template');
+    }
+
+    private function createTemplate(string $templateType, string $name) {
         $template = QuotationTemplate::create([
-            'name' => 'ACCREDITATION',
-            'service_type' => 'LOGISTICS',
+            'name' => $name,
+            'service_type' => $templateType,
             'is_active' => true
         ]);
 
@@ -38,7 +43,10 @@ class TemplateSeeder extends Seeder
             $templateCharge->allowedReceiptCharges()->attach(fake()->randomElements($receiptChargeIds, random_int(2, $receiptChargesCount)));
         }
 
-        $quotationFields = QuotationField::logisticsFields()->pluck('id')->toArray();
+        $quotationFields = match($templateType) {
+            'LOGISTICS' => QuotationField::logisticsFields()->pluck('id')->toArray(),
+            'REGULATORY' => QuotationField::regulatoryFields()->pluck('id')->toArray(),
+        };
 
         $template->quotationFields()->attach(
             fake()->randomElements($quotationFields, random_int(2, count($quotationFields)))
