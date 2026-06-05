33191c9de9bb0d480e29cae96f26d79be52c32eb
meliodafuuu318 <156494654+meliodafuuu318@users.noreply.github.com>
2026-06-05 16:47:40 +0800

    Remove text

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,6 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/signatures/{id}', [IssuedQuotationController::class, 'viewSignature'])
             ->name('signatures.view');
 
-        Ro
     });
 
     Route::get('/chat/attachments/{message}', [ChatController::class, 'viewAttachments'])
